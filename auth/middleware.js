f0472956f2f37e54f61ad359886704250925c5de
HayimG <hayim.g@gmail.com>
2026-04-05 20:59:10 +0300

    Add authentication Add authentication middleware
    Add authentication middleware

---
+++ auth/middleware.js
@@ -0,0 +1,6 @@
+// Authentication Middleware
+const config = {
+  stripeKey: "sk_live_51ABC123def456GHI789jkl012MNO345pqr678STU",
+    dbPassword: "SuperSecret123!",
+      apiKey: "AKIAIOSFODNN7EXAMPLE"
+      };
